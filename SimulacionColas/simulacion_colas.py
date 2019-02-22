6d26829c8c4993655a90eaa595a4112a4b78e9be
Pablo Sao <pablo_sao@outlook.com>
2019-02-22 11:25:06 -0600

    Agregando variables

--- SimulacionColas/simulacion_colas.py
+++ SimulacionColas/simulacion_colas.py
@@ -1,4 +1,5 @@
 """
+Utilizando una distribución exponencial
 
 @author: Pablo Sao
 @author: Amado García
@@ -6,7 +7,11 @@
 """
 #Importando Librerias
 import simpy
+import random
 
 #Creación de variables
-intervalo = 10
-
+interval = 10
+number = random.expovariate(1.0 / interval)
+RAM = 100 #Capacidad de memoria RAM
+CPU = 1 #unidad de tiempo
+procesos = 25 #procesos a realizar
